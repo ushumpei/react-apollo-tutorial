29093dfd020ce87fb28c38741ab8866ac6585a97
ushumpei <shumpei.uzawa@gmail.com>
2017-12-06 16:56:52 +0800

    End Step 3.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import { makeExecutableSchema, addMockFunctionsToSchema, } from 'graphql-tools';
 import { typeDefs } from './schema';
 import MockLink from './MockLink';
 
+import { ApolloProvider } from 'react-apollo';
+import UsersList from './UsersList';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,28 +21,21 @@ const link = new MockLink({ schema });
 const cache = new InMemoryCache();
 const client = new ApolloClient({ link, cache });
 
-const query = gql`query {
-  users {
-    id
-    name
-  }
-}`;
-client.query({ query })
-  .then(console.log)
-  .catch(console.error);
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <UsersList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ client/src/UsersList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import './UsersList.css';
+
+const UsersList = ({ data: { loading, error, users }}) => {
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return <ul className="users-list">
+    { users.map(u => <li key={u.id}>{u.name}</li>) }
+  </ul>;
+};
+
+export const usersListQuery = gql`query {
+  users {
+    id
+    name
+  }
+}`;
+
+export default graphql(usersListQuery)(UsersList);
